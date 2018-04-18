e945bac09ee0f008c83a4cd780dd26db7e2b16b3
the f <feldrim@gmail.com>
2018-04-18 21:40:12 +0300

    Added empty test projct

---
+++ ProblemA_TheSeat.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProblemA_TheSeat.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProblemA_TheSeat.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6dbcda5c-87ce-4825-83d8-10d92a1002cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProblemA_TheSeat.Tests/TheSeatTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProblemA_TheSeat.Tests
+{
+   [TestClass]
+   public class TheSeatTests
+   {
+      [TestMethod]
+      public void SampleDataTest()
+      {
+
+      }
+   }
+}
