31e997b7a6a890f2efc4ece098acc55f056cc0a7
the f <feldrim@gmail.com>
2018-04-18 22:45:37 +0300

    Added first test code

--- ProblemA_TheSeat.Tests/TheSeatTests.cs
+++ ProblemA_TheSeat.Tests/TheSeatTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ProblemA_TheSeat.Tests
 {
@@ -9,7 +8,13 @@ namespace ProblemA_TheSeat.Tests
       [TestMethod]
       public void SampleDataTest()
       {
+         const string input = "UUUDDUDU";
+         var sequence = new Sequence(input);
 
+         var policyFactory = new PolicyFactory(sequence);
+         var results = policyFactory.GetResults();
+
+         Assert.AreEqual(new[] {6, 7, 4}, results);
       }
    }
 }
