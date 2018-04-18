b9745f15666c422c8929736978cced46045a18b1
the f <feldrim@gmail.com>
2018-04-18 23:57:30 +0300

    Simplified Sequence class

--- ProblemA_TheSeat/Sequence.cs
+++ ProblemA_TheSeat/Sequence.cs
@@ -8,14 +8,9 @@ namespace ProblemA_TheSeat
       public Sequence(string input)
       {
          if (input.Length < 2 || input.Length > 1000) throw new ArgumentOutOfRangeException(nameof(input), input.Length, "Input string length must be between 2 and 1000.");
-         InitialPosition = input[0];
-         var temp = new char[input.Length - 1];
-         input.CopyTo(1, temp, 0, input.Length - 1);
-         Preferences = temp;
+         Sequences = input.ToCharArray();
       }
 
-      public char InitialPosition { get; }
-
-      public IEnumerable<char> Preferences { get; }
+      public IEnumerable<char> Sequences { get; }
    }
 }
