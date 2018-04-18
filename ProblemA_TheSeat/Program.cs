1751cded8afee30faa8dda5cfd8ed5a5c2124277
the f <feldrim@gmail.com>
2018-04-18 21:33:53 +0300

    Add project files.

---
+++ ProblemA_TheSeat/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemA_TheSeat
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+      }
+   }
+}
